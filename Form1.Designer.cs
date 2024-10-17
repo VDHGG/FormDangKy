d2553de967dd9c61358117047947fb1a224b2c26
VDHGG <vuduchuy2072004@gmail.com>
2024-10-17 15:25:59 +0700

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace FormLoginWithCode
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.TextTenDangNhap = new System.Windows.Forms.TextBox();
+            this.TextEmail = new System.Windows.Forms.TextBox();
+            this.TextMatKhau = new System.Windows.Forms.TextBox();
+            this.HienThi = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(328, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(270, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ĐĂNG KÝ TÀI KHOẢN";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(66, 87);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(144, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Tên Đăng Nhập:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(66, 146);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(68, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Email: ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(66, 209);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(94, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Mật Khẩu:";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(229, 281);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(191, 29);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "ĐĂNG KÝ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // TextTenDangNhap
+            // 
+            this.TextTenDangNhap.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TextTenDangNhap.Location = new System.Drawing.Point(229, 87);
+            this.TextTenDangNhap.Name = "TextTenDangNhap";
+            this.TextTenDangNhap.Size = new System.Drawing.Size(550, 27);
+            this.TextTenDangNhap.TabIndex = 5;
+            // 
+            // TextEmail
+            // 
+            this.TextEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TextEmail.Location = new System.Drawing.Point(229, 143);
+            this.TextEmail.Name = "TextEmail";
+            this.TextEmail.Size = new System.Drawing.Size(550, 27);
+            this.TextEmail.TabIndex = 6;
+            // 
+            // TextMatKhau
+            // 
+            this.TextMatKhau.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.TextMatKhau.Location = new System.Drawing.Point(229, 206);
+            this.TextMatKhau.Name = "TextMatKhau";
+            this.TextMatKhau.Size = new System.Drawing.Size(550, 27);
+            this.TextMatKhau.TabIndex = 7;
+            // 
+            // HienThi
+            // 
+            this.HienThi.AutoSize = true;
+            this.HienThi.BackColor = System.Drawing.Color.Lime;
+            this.HienThi.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.HienThi.Location = new System.Drawing.Point(224, 354);
+            this.HienThi.Name = "HienThi";
+            this.HienThi.Size = new System.Drawing.Size(0, 25);
+            this.HienThi.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(912, 501);
+            this.Controls.Add(this.HienThi);
+            this.Controls.Add(this.TextMatKhau);
+            this.Controls.Add(this.TextEmail);
+            this.Controls.Add(this.TextTenDangNhap);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox TextTenDangNhap;
+        private System.Windows.Forms.TextBox TextEmail;
+        private System.Windows.Forms.TextBox TextMatKhau;
+        private System.Windows.Forms.Label HienThi;
+    }
+}
+
